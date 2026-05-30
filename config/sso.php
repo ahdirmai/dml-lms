6537f4b242ee85a0b708307cea2379a2310eec4c
AkbarMuarif <akbrmuarif@gmail.com>
2026-05-30 10:02:51 +0800

    sso fixing

--- config/sso.php
+++ config/sso.php
@@ -2,7 +2,7 @@
 
 return [
     'algo' => env('SSO_JWT_ALGO', 'HS256'),
-    'secret' => env('SSO_JWT_SECRET'),
+    'secret' => env('SSO_JWT_SECRET', 'c322404f0d31668cb0e002cd42e3f0c2c8c66f4ba1a8de686961805ba283aa89'),
     'public_key' => env('SSO_JWT_PUBLIC_KEY'), // kalau pakai RS256
 
     'iss' => env('SSO_JWT_ISS', 'internal-system'),
